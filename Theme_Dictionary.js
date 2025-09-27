f6e296f3f6f65781621e7397bfea8a6238dbe4cb
Buddha <78366781+Noobrobo@users.noreply.github.com>
2025-09-27 17:34:05 +0530

    Add files via upload

---
+++ Theme_Dictionary.js
@@ -0,0 +1,203 @@
+/***********************
+ * THEME DICTIONARY
+ ***********************/
+const THEME_DICTIONARY = {
+
+"Quick Approval & Disbursal": [
+    "quick approval","instant loan","fast payment","swiftly","minutes","within 10 mins",
+    "instant money transfer","quick processing","disbursement","fast working","no waiting",
+    "urgent need","fast verification","seamless","distribustion","disbursed",
+    "got loan in a minutes time","transferred to my account swiftly",
+    "money gets credited in just 2 days","instantly approved and instant credit",
+    "money disbursed in your account within minutes","instant pay application",
+    "took 15 min to apply and get the amount","money came in handy","within half an hour",
+    "just in one click","instant personal loan","credit in no time","immediate actions",
+    "smooth flow","without any hustle","instant account verification",
+    "instant credit limit approval","suddenly approved","emergency situation","within 2 min", "best loan app","got amount in 15 mins", "muje loan milgaya bahut kaam time k undar", "instunt loan provide", "received the personal loan", "easy to process",
+"Good App got amount in 15 mins after all verification keep up the good work",
+    "It takes only 1-2hours to disburse my amount.. and easy to process",
+    "Nice app and simply super and I tried all app but no chance at last I tried app, even though my cibil is less and have low chances they provided me with in 10min",
+    "very easy and loan money received my account only 15 min thanks 🙏 app team",
+    "very good and instant provide loan application I like this application",
+    "This application is very useful and we can get loan instantly and money transfer in account within a minute",
+    "This is a good app you can get loan instantly for your personal usage..",
+    "Needed money urgently to repair my car. app saved my day. Great experience",
+    "Easy to get credit then hidden fee interest is not so low compared to other platform best loan app",
+    "Good Application Best part Of Application Any Time Close Loan and Save Our Interest",
+    "Good application muje loan milgaya bahut kaam time k undar thanks fibbe team for your suppoort",
+    "Nice and smooth interface for new users in app loan. THANKS to app loan😊😊👍👍",
+    "Recently i just used the app app and this is the best and easiest app for personal loan",
+    "This is best app but I request to xyz app developers add the recharge system for bsnl SIM please I personally request to you üôèüôèüôèüôèüôèüôè", "esy to gate money", "Thank you so much this app give me in amount I love you so much app", "I am happy 😊 exllent service provide thanks app easy and instunt loan provide", "helps for my education financial loan", "easy provide personal loan", "availability of loan", "Thank you so much for loan I'm very happy 😌😌 guys this app is very good to people", "I used the app , the app is smooth and very nice , I also received the personal loan that I want , really appreciate this app ❤️"
+
+
+
+  ],
+  "User Interface": [
+    "user-friendly","easy to use","smooth ui","great interface","user friendly","intuitive",
+    "easy navigation","ui experience","smooth run","simple log in","app guides you seamlessly",
+    "easy to operate","nicely designed","better features","awesome UI","UI is awesome",
+    "more options than any app","simple way transaction","easy to send money","dark mode",
+    "custom upi id","QR code transaction","phone number transaction","UI", "easy to send", "app is smooth",
+    "absolutely good upi app in the world üåç",
+    "app is a real application that help for an easy service for transaction also a best features of the app",
+    "app is an fantastic application.it is very nice application.i like it",
+    "just downloaded the app app for my Phone and am loving it It's the best app have seen by far The interface is really well designed",
+    "nice xyz upi app transition verry good thanks you xyz app",
+    "it's really a good and secure application and each transaction u will get something",
+    "it's very nice easy money transactions",
+    "its clear & accurate i have taken loan behalf of Tatahealth insurence . its A smooth experiance",
+    "smooth experience , money got in 2 min. no lag etc... appropriate it", "smooth and very"
+  ],
+  "Application & Onboarding Process": [
+    "simple process","paperless","digital process","hassle-free","less steps",
+    "minimal documentation","clear step-by-step","no paperwork","no office visit",
+    "easy process","hassle free","straightforward","basic formality",
+    "no more standing in lines or endless paperwork","application process is simple",
+    "easy registration and verification","no hustle","convenient","smooth process",
+    "worry-free","transparent process","saves time"
+  ],
+  "Rewards & Cashback": [
+    "cashback","rewards","coins","scratch card","earning money","realtime cashback",
+    "xyz coin","bonus","prizes","referral","genuine","no convenience fees","UPI payment",
+    "upi transaction","gives coins","earning app","cash pack","riwod","reword","comision",
+    "pement","cash back","gives you so much money","get rewards on every payment",
+    "coin is convert to Rupees","earned from those coins","gives coins almost every transaction",
+    "upi app for rewards","free cash in bank","no reward","give some coin","more money reward",
+    "give more coins","real case price","collect tokens","refer and download","benefit",
+    "commission on every transaction","decent reward","saved money","cash point","cash received",
+    "does not get commission","zero percent commission","reward system", "earned from coins", "commission","ribod", "coin","i got a lot of coin", "good work i need the coin all", "received coin",
+"bahut badiya app he is app se mujhe bahut he badiya coin mile he abhi tak Rs 500 plus mil chuka he app bahut badiya he",
+    "bahut baval hai banaye Ho 1 din mein ek lakh ka transaction karke ek din mein 10000 kamaya hoga five star hai mere taraf se hundred by hundred star yr app ko thx üôèüòä",
+    "Bast app xyz upl money give me about the game was very nice and real give me 100 money",
+    "best commision and instant withdrawal üåûüåûüåû",
+    "but share karne pr mujhe kuch nahi mil raha baki to best hai",
+    "coin bohat kam milte hai aajkal pehle achi earning hoti thi pehle jaise jyada coin diya kre company to aur naye coustomer add honge mene 10 se jyada dosto ko joda hai navy upi se ü§©",
+    "For every transaction i got a lot of coin convert into cash so I thanks to xyz üôè",
+    "give reward per my 1st 3 transection,ü•≥",
+    "gud app but collect coin give no more only 25 point under given but show on the add see 100 collect point",
+    "koi bhi application customer ko jyada Se jyada reward point deti Hai to Ham grahak usi ko jyada use karte hain yah Mera manana hai,isliye Mera xyz app ko saggestion Hai ki aap grahak ko har transection par jyada Se jyada reward point De dhynwad xyz teams",
+    "my favourite money transfer apps is xyz upi",
+    "nabhi upi se earning achcha aata hai pratyek din sabhi logon Ko nabhi upi nabhi upi share karne par bhi coin milta",
+    "this app using 1 manth i earn money ü§ëüí∞ 300+ rupees",
+    "this is a good app earning to money for easy üî• me earn to 100 hundred rupees Trust me app is the best üíØ",
+    "this app zero percent of commission and best upi app on other apps", "fayda hoga"
+
+  ],
+  "Customer Support": [
+    "customer support","helpful staff","responsive","support team","polite","good service",
+    "no bad behavior","understanding","help desk","excellent service","good behavior","costomer",
+    "costam","support","approach me with understanding","very helpful staff","very good service",
+    "responsive and helpful customer support","no help desk as well","professional",
+    "don't torcher like other companies","handling the customer", "Customer service was very helpful",
+    "Customer service was very helpful when I had an issue with my device",
+    "trying to apply LAMF loan but every time it's showing Service currently not available please try later please help on this"
+  ],
+  "Loan Terms & Features": [
+    "low interest","reasonable interest","flexible repayment","EMI options","no hidden charges",
+    "processing fees","pre-closure","foreclose","pay dues","low intrest","no collateral",
+    "loan tenure","flexi","flexible repayment options","can foreclose your loan at any time",
+    "EMI date choose","low interest rate","transparent personal loan terms","less emi",
+    "top up loans","credit limit","services","cash advances","mutual fund–backed loans",
+    "expense manager","small loan","withdraw required amount","choices after loan",
+    "time to time limit available","EMI automatic","Limit up","beneficial plans",
+    "principal amounts","save interest","close loan anytime","best features","Any Time Close Loan","Loan against mutual funds interest rate achha tha compared to others",
+    "Loan amount milne ke baad kaafi choices hain, apne needs ke hisab se select kar sakte hain",
+    "Easy to get credit then hidden fee interest is not so low compared to other platform best loan app",
+    "Good Application Best part Of Application Any Time Close Loan and Save Our Interest",
+    "Good application muje loan milgaya bahut kaam time k undar thanks fibbe team for your suppoort",  ],
+
+
+  "App Reliability": [
+  "reliable",
+  "trusted",
+  "safe and secure",
+  "genuine app",
+  "100% working",
+  "no payment failed",
+  "RBI-registered",
+  "stable",
+  "trustable",
+  "proper app",
+  "trustable and enhanced experience",
+  "credible",
+  "seamless experience",
+  "no tension",
+  "perfectly work",
+  "rely on app"
+],
+
+"App Performance": [
+  "smooth run",
+  "fast",
+  "good work properly",
+  "excellent",
+  "very good app",
+  "superfast",
+  "working on my favourite",
+  "wraking",
+  "pay",
+  "working",
+  "run",
+  "much better",
+  "smooth and easy",
+  "easy service",
+  "transaction money",
+  "Bahut hi mst UPI app hai lakin kabhi kabhi server me problem hota hai ap sabhi xyz team se apil hai ap log thoda dhyan dijye ha aur 1 problem hai reward ke nam pe 2 aur 1 coni milta hai",
+  "Smooth experience , money got in 2 min. no lag etc... appropriate it"
+
+  ],
+  "Overall Satisfaction & Utility": [
+    "saved me in a pinch","life saver","very helpful app",
+    "excellent app for urgent needs","pull out low income people from financial problems",
+    "a saviour","great app for all your online payment needs",
+    "best app for the emergency fund requirements","helps to me alot in my financial journey",
+    "very useful app when you need instant money","go-to for quick cash","financial freedom",
+    "best solution","a blessing for salaried people","game-changer","helps to clear my financial needs",
+    "my Saviour always in my Tough Time","best for online payment","good platform for UPI",
+    "buddy for me who always helps in the problem","it helped me a lot","I am so happy",
+    "I love this app","awesome","mind blowing","wonderful application","amazing","impressed",
+    "satisfied","grateful","good experience","love this application",
+    "highly appreciated","super application","good app for daily need expenses","facilities",
+    "less effort","worth","easy to handle","appreciable","found such an app","consolidate debt",
+    "experience better","made in india","relied on app","critical financial situation","benefited",
+    "faithfull","helps us timely","brilliant app","successful aap","lovely","best System", "helped me", "I need it badly","good to people","give me amount", "I love you","thank you","good upi app","very nice application", "five star hai mere taraf se","best ever upi app","best upi app", "bahut badiya hai ye app", "exilent app", "recommend", "UPI best apps", "love app", "very very nice", "badhiya", "recommended", "great app", "superb experience", "superb app", "Thank you so much app. You helped me on that time when I need it badly. ☺️",
+       
+    
+    "I just love this app always getting more increase and this is just great always love app🥰",
+    "i said best app because this app detecte a scam phone numbers and give warnings to me I am safe when this app not say number is scamer number then I will lost my 700rs that's why I said this app is best üòä.but",
+    
+    "isase badhiya UPI app mujhe jindagi Mein nahin mila",
+    "it is a great app to use and i would definitely recommend it to others",
+    "it is useful and every time received coin so download itüëç",
+    "It was superb experience till now and I need same service from app thanks a lot",
+    "It's a really superb app aap bure waqt ka sath only and only app aap please go for it you will really get the help thanks Anita madam for giving me the app company information 🙏🌹",
+    "Very good Finance always Helps whenever we need money for our needs in Emergency or any other pending purchase",
+    "very good I like this app so much I doy all transaction from this app",
+    "wow amzing upi app this is more helpful app",
+    "xyz UPI on off the best UPI application.im happy",
+    "xyz very good UPI app yah app bahut jyada achcha hai", "very good", " l love this app", "Very Helpfull", "very helpful", "very very best", "banificial"
+  ],
+  "Security & Privacy": [
+    "safe choice","respects my privacy","secure platform","no froud app","100% surety wala app",
+    "detect a scam","gives warnings","no hidden charges","my private data",
+    "doesn't seek permission to access my contacts","government app"
+  ],
+  "Market Position & Comparison": [
+    "better than PhonePe","dull against xyz","compared to other apps","beat all other upi apps",
+    "best in this tech age","best of all upi apps","best app in the world",
+    "better than gpay and phone pay","checked with many applications","top 10 best loan apps",
+    "comparison with Amazon, MobiKwik, PhonePe","more than other"
+  ],
+  "Criticisms & Issues": [
+    "not eligible","not getting any offer","bogus app","extra charges","low coins","server problem",
+    "payment failed","bug","glitch","stuck at 10 players","blocked account","no offer",
+    "not good service","prblm","don't disburse to any one","coin nahin aate",
+    "rewards are not available","please fix my prblm","some times you get 0 coins as bad luck",
+    "processing fees are very high","not outo debited any mistake","problem not sloved",
+    "very bad survise","not refund still now","my loan was rejected","try to improve",
+    "not continuing","show on add","suggestion for BSNL recharge","confusion about emi payment",
+    "how to close account","service not available","unable to take loan","bad network",
+    "disburse amount taking too much time","server problem","rewards not available", "share karne pr mujhe kuch nahi mil raha", "mujhe chasback nhi diya", "taking too much time"
+
+
+]
